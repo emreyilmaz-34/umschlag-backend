e2dd574ea03260a38b7c491186938952ca8a2f35
emre <emre.yilmaz@nextlua.com>
2021-11-07 02:51:23 +0300

    update

---
+++ Umschlag-Backend.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+using Umschlag_Backend.Core.Models;
+using System.Collections.Generic;
+
+namespace Umschlag_Backend.Core.Repositories
+{
+    interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
