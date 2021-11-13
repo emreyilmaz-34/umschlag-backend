d6db089f2ab392a5515edacfcfbe06a6c743b238
emreyilmaz34 <emreyilmaz.34@yandex.com>
2021-11-13 19:38:44 +0300

    mvc project done

---
+++ Umschlag.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Umscghlag.Web.Filters;
+using Umschlag.Web.DTOs;
+using Umschlag_Backend.Core;
+using Umschlag_Backend.Core.Services;
+
+namespace Umschlag.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+            => View(_mapper.Map<IEnumerable<CategoryDto>>(await _categoryService.GetAllAsync()));
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id)
+            => View(_mapper.Map<CategoryDto>(await _categoryService.GetByIdAsync(id)));
+
+        [HttpPost]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            _categoryService.Update(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
+        }
+        
+        [ServiceFilter(typeof(NotFoundFilter))]
+        public IActionResult Delete(int id)
+        {
+            _categoryService.Remove(_mapper.Map<Category>(_categoryService.GetByIdAsync(id).Result));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Umschlag.Web/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Umscghlag.Web.Filters;
+using Umschlag_Backend.Core.Repositories;
+using Umschlag_Backend.Core.Services;
+using Umschlag_Backend.Core.UnitOfWork;
+using Umschlag_Backend.Data;
+using Umschlag_Backend.Data.Repositories;
+using Umschlag_Backend.Data.UnitOfWorks;
+using Umschlag_Backend.Service.Services;
+
+namespace Umschlag.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<NotFoundFilter>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddControllersWithViews();
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IService<>), typeof(Service<>));
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:SqlConStr"].ToString(), o =>
+            {
+                o.MigrationsAssembly("Umschlag-Backend.Data");
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
